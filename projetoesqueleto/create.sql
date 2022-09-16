8ed8c72b43f4c63cff39405aec80da3a60a858c4
Bissolion <luizfelipegolopes@gmail.com>
2022-09-16 17:57:06 -0300

    commit projeto esqueleto progresso login

---
+++ projetoesqueleto/create.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE cavanhaque;
+
+CREATE USER 'dev'@'localhost' IDENTIFIED BY '123'; 
+GRANT ALL PRIVILEGES ON cavanhaque.* TO 'dev'@'localhost';
+
+CREATE TABLE usuario ( 
+    id int NOT NULL AUTO_INCREMENT, 
+    nome varchar(100) NOT NULL, 
+    email varchar(100) NOT NULL, 
+    senha varchar(50) NOT NULL, 
+    primary key(id) 
+);
+
+INSERT INTO usuario (nome, email, senha)
+VALUES ('admin','admin@admicavanhaque.com.cdb', 'cdb0108');
--- projetoesqueleto/register.php
+++ projetoesqueleto/register.php
@@ -1,4 +1,4 @@
-<?php include "conectamysqlcdb.inc"; ?>
+<?php include "conectamysqlcdb.php"; ?>
 <html>
    <head>
         <title>Cavanhaque de Bode</title>
@@ -7,40 +7,60 @@
     </head>
     <body bgcolor="black">
     <?php
-        $nome = $_POST["nome"]; 
-        $email = $_POST["email"];
-        $senha = $_POST["senha"];
-        $confirmsenha = $_POST["confirmsenha"];        
         
-        $erro = 0;
-
-        if(empty($nome)){
-            echo "<p> Por favor, preencha o campo \"nome\".<br></p>";
-                $erro = 1;
-        }
-
-        else if(strlen($email) < 10 or strstr($email, '@') == false){
-            echo "<p>Por favor, preencha o e-mail corretamente.<br></p>";
-            $erro = 1;
-        }
-        else if(empty($senha) or empty($confirmsenha)){
-            echo "<p>Por favor, preencha o campo \"senha\".<br></p>";
-            $erro = 1;    
-        }
-        else if($senha != $confirmsenha){
-            echo "<p>Por favor, verifique se as senhas são iguais.<br></p>";
-            $erro = 1; 
-        }
-        else if($erro == 0){
-            $sql = "INSERT INTO usuario (nome, email, senha)";
-            $sql .= "VALUES ('$nome','$email','$senha');";  
-            mysqli_query($mysqli,$sql); 
-            mysqli_close($mysqli);
-            header('Location mainpage.html');
-            echo "<p>Registro concluído com sucesso!</p>";
-            echo "<a class='burrao' href='mainpage.html'> Voltar</a>";
+        $operacao = $_POST["operacao"];
+        if($operacao == "cadastro"){
+            $nome = $_POST["nome"]; 
+            $email = $_POST["email"];
+            $senha = $_POST["senha"];
+            $confirmsenha = $_POST["confirmsenha"];        
             
+            $erro = 0;
+
+            if(empty($nome)){
+                echo "<p> Por favor, preencha o campo \"nome\".<br></p>";
+                    $erro = 1;
+            }
+
+            else if(strlen($email) < 10 or strstr($email, '@') == false){
+                echo "<p>Por favor, preencha o e-mail corretamente.<br></p>";
+                $erro = 1;
+            }
+            else if(empty($senha) or empty($confirmsenha)){
+                echo "<p>Por favor, preencha o campo \"senha\".<br></p>";
+                $erro = 1;    
+            }
+            else if($senha != $confirmsenha){
+                echo "<p>Por favor, verifique se as senhas são iguais.<br></p>";
+                $erro = 1; 
+            }
+            else if($erro == 0){
+                $sql = "INSERT INTO usuario (nome, email, senha)";
+                $sql .= "VALUES ('$nome','$email','$senha');";  
+                mysqli_query($mysqli,$sql); 
+                mysqli_close($mysqli);
+                echo "<p>Registro concluído com sucesso!</p>";
+                echo "<a class='burrao' href='mainpage.html'> Voltar</a>";
+                
+            }
         }
+        else if($operacao == "login"){
+            
+            if(empty($_POST['email']) or empty($_POST['email'])){
+                echo "deu ruim";
+            }
+
+            $email = mysql_real_escape_string($mysqli, $_POST['email']);
+            $senha = mysql_real_escape_string($mysqli, $_POST['senha']);
+
+            $query = "SELECT id FROM usuario WHERE email = '$email' and senha '$senha'";
+            $result = mysqli_query($mysqli, $query);
+
+            $row = mysqli_num_rows($result);
+
+
+        }
+
     ?>
     </body>
 </html>
